e0b5d9c4fe37a401ac6251ed93d845f2753da423
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-10-25 15:15:48 -0300

    Log in

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from item.models import Category, Item
 
-from . forms import SignupForm
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -16,7 +16,6 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
-
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -27,7 +26,6 @@ def signup(request):
             return redirect('/login/')
     else:
         form = SignupForm()
-    
 
     return render(request, 'core/signup.html', {
         'form': form
